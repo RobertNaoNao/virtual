be944b711616f653a44fc99d1c4120f0105cd076
zhaonaonao <zhaonaonao@beisen.com>
2017-05-16 14:07:02 +0800

    练习的虚方法

---
+++ VirtualP/VirtualP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualP
+{
+    class Program:BaseClass
+    {
+        static int z = 100;
+        public override int Add(int x, int y)
+        {
+            return base.Add(x, y)+z;
+        }
+        static void Main(string[] args)
+        {
+            BaseClass baseclass = new BaseClass();
+            Console.WriteLine(baseclass.Add(10,10));
+            Console.WriteLine(new Program().Add(10,10));
+            Console.ReadKey();
+        }
+    }
+    class BaseClass {
+        public virtual int Add(int x,int y) {
+            return x + y;
+        }
+    }
+}
